59b24e04747df8526823d107e039b24a05365d71
Matheus Maia <roxx77344@gmail.com>
2022-06-17 16:41:37 -0300

    feat: create simple get method on HttpClient

--- src/http.rs
+++ src/http.rs
@@ -2,36 +2,43 @@ use crate::endpoints::Endpoint;
 use reqwest::header;
 
 pub struct HttpClient {
-		_client: reqwest::Client,
-		_base_url: String,
+    _client: reqwest::Client,
+    _base_url: String,
 }
 impl HttpClient {
-		pub fn new(base_url: String, headers: header::HeaderMap) -> HttpClient {
-				let _client = reqwest::Client::builder()
-						.default_headers(headers)
-						.build()
-						.unwrap();
-				HttpClient {
-						_client,
-						_base_url: base_url,
-				}
-		}
+    pub fn new(base_url: String, headers: header::HeaderMap) -> HttpClient {
+        let _client = reqwest::Client::builder()
+            .default_headers(headers)
+            .build()
+            .unwrap();
+        HttpClient {
+            _client,
+            _base_url: base_url,
+        }
+    }
 
-		pub async fn post<T: serde::Serialize>(
-				self,
-				endpoint: Endpoint,
-				body: Option<T>,
-		) -> reqwest::Response {
-				let mut response =
-						self._client
-								.post(format!("{}{}", self._base_url, endpoint.as_str()));
-				if let Some(body) = body {
-						response = response.json(&body);
-				};
-				response.send().await.unwrap()
-		}
+    pub async fn post<T: serde::Serialize>(
+        self,
+        endpoint: Endpoint,
+        body: Option<T>,
+    ) -> reqwest::Response {
+        let mut response = self
+            ._client
+            .post(format!("{}{}", self._base_url, endpoint.as_str()));
+        if let Some(body) = body {
+            response = response.json(&body);
+        };
+        response.send().await.unwrap()
+    }
+    pub async fn get(self, endpoint: Endpoint) -> reqwest::Response {
+        self._client
+            .get(format!("{}{}", self._base_url, endpoint.as_str()))
+            .send()
+            .await
+            .unwrap()
+    }
 }
 #[derive(Debug)]
 pub struct HttpError {
-		pub status: u16,
+    pub status: u16,
 }
