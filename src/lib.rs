7321f469e0e488205ea935ceb5db88ea27bcacd4
Matheus Maia <roxx77344@gmail.com>
2022-06-17 16:15:48 -0300

    refactor: move http module to its own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod endpoints;
+mod http;
 mod response;
 
 use public_key::PublicKeyClient;
@@ -42,46 +43,6 @@ impl PagseguroSDK {
         PagseguroSDK {
             public_key: PublicKeyClient::new(_client, _base_url.to_string()),
         }
-    }
-}
-
-pub mod http {
-    use crate::endpoints::Endpoint;
-    use reqwest::header;
-
-    pub struct HttpClient {
-        _client: reqwest::Client,
-        _base_url: String,
-    }
-    impl HttpClient {
-        pub fn new(base_url: String, headers: header::HeaderMap) -> HttpClient {
-            let _client = reqwest::Client::builder()
-                .default_headers(headers)
-                .build()
-                .unwrap();
-            HttpClient {
-                _client,
-                _base_url: base_url,
-            }
-        }
-
-        pub async fn post<T: serde::Serialize>(
-            self,
-            endpoint: Endpoint,
-            body: Option<T>,
-        ) -> reqwest::Response {
-            let mut response =
-                self._client
-                    .post(format!("{}{}", self._base_url, endpoint.as_str()));
-            if let Some(body) = body {
-                response = response.json(&body);
-            };
-            response.send().await.unwrap()
-        }
-    }
-    #[derive(Debug)]
-    pub struct HttpError {
-        pub status: u16,
     }
 }
 
